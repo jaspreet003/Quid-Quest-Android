3995adfffc55a9c8652298a9b516d42dc96300cc
Laura Silva <laura@git.com>
2023-10-22 09:02:31 -0400

    Manage Departments layout First Draft

---
+++ app/src/main/java/com/androidclass2023/quidquest/ManageDepartments.java
@@ -0,0 +1,14 @@
+package com.androidclass2023.quidquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageDepartments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_departments);
+    }
+}

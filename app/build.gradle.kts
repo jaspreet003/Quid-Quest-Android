42ccf531babc71b918cb527ca9eabd56ea8b92a7
Laura Silva <laura@git.com>
2023-11-01 20:37:18 -0400

    Onboarding Layout First Draft

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.androidclass2023.quidquest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.androidclass2023.quidquest"
---
+++ app/src/main/java/com/androidclass2023/quidquest/OnboardingBankDetails.java
@@ -0,0 +1,14 @@
+package com.androidclass2023.quidquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnboardingBankDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_bank_details);
+    }
+}

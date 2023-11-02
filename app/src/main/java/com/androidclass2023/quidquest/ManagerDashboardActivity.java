0d6f84fc5ef0eed825da02c93b8aeb634611cbd8
jaspreet <jkaur003@gmail.com>
2023-11-02 12:20:14 -0400

    dashboard layout done

--- app/src/main/java/com/androidclass2023/quidquest/ManagerDashboardActivity.java
+++ app/src/main/java/com/androidclass2023/quidquest/ManagerDashboardActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import adapters.DashboardViewPagerAdapter;
 
-public class ManagerDashboardActivity extends AppCompatActivity {
+public class ManagerDashboardActivity extends AppCompatActivity implements View.OnClickListener {
+
+   Button btnWeekly, btnMonthly, btnLastMonth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_dashboard);
@@ -36,5 +40,39 @@ public class ManagerDashboardActivity extends AppCompatActivity {
                             break;
                     }
                 }).attach();
+
+        initialize();
     }
+
+    private void initialize() {
+        // Configure the variables
+        btnMonthly = findViewById(R.id.btnMonthly);
+        btnWeekly = findViewById(R.id.btnWeekly);
+        btnLastMonth = findViewById(R.id.btnLastMonth);
+
+        //setting listener to buttons
+        btnWeekly.setOnClickListener(this);
+        btnMonthly.setOnClickListener(this);
+        btnLastMonth.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btnWeekly){
+
+        }else if (v.getId() == R.id.btnMonthly){
+
+        }else if(v.getId() == R.id.btnLastMonth){
+
+        }
+    }
+
+    public void setTimeButtonStyle(Button b){
+
+    }
+
 }

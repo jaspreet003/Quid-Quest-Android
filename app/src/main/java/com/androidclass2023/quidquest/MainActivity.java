d3b1a909172c06382231e08385a06ab0299b3557
Sandeep Singh <sandeepcn998@gmail.com>
2023-12-06 22:17:25 -0500

    move menu to ManagerDashboardActivity

--- app/src/main/java/com/androidclass2023/quidquest/MainActivity.java
+++ app/src/main/java/com/androidclass2023/quidquest/MainActivity.java
@@ -2,55 +2,19 @@ package com.androidclass2023.quidquest;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
         TextView tvLogin = findViewById(R.id.tvLogin);
-
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        navigationView.setNavigationItemSelectedListener(item -> {
-            // Handle navigation view item clicks here.
-            int id = item.getItemId();
-
-            if (id == R.id.nav_dashboard) {
-                Intent intent = new Intent(MainActivity.this, ManagerDashboardActivity.class);
-                startActivity(intent);
-            } else if (id == R.id.nav_expense) {
-                Intent intent = new Intent(MainActivity.this, Expense_Screen.class);
-                startActivity(intent);
-            } else if (id == R.id.nav_settings) {
-                Intent intent = new Intent(MainActivity.this, Seetings_Menu.class);
-                startActivity(intent);
-            } else if (id==R.id.nav_employees) {
-                Intent intent = new Intent(MainActivity.this, EmployeeLIstScreen.class);
-                startActivity(intent);
-            }
-
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return true;
-        });
 
         tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,11 +25,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
